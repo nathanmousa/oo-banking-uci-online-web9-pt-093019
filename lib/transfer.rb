1c29f12fe50749e521ca9ec74cecb40095329562
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-07 19:10:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && status = "pending" && sender.balance >= amount
+    if valid? && status == "pending" && sender.balance >= amount
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
